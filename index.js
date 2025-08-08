8cee720fb83e5a76ebc60e9887fa59fb0b3a8392
ashkapile <ashvinikapile123@gmail.com>
2025-08-08 18:23:52 +0530

    pushed nodeJS code

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Node.js App!\n');
+  let filePath = './public/index.html';
+  let contentType = 'text/html';
+
+  if (req.url === '/style.css') {
+    filePath = './public/style.css';
+    contentType = 'text/css';
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Error loading file');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
 });
 
 server.listen(PORT, () => {
